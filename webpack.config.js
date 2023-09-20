19ad11639cc6d7f8106b56d0dbad55121c3391e6
Mikkolla <kamenevkola05@gmail.com>
2023-09-20 19:08:21 +0300

    init it

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const config = {
+	mode: 'production',
+	entry: {
+		index: './src/js/index.js',
+		 carousel: './src/js/owl.carousel.min',
+		// about: './src/js/about.js',
+	},
+	output: {
+		filename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+};
+
+module.exports = config;
